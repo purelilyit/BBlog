aa3079ab9451f4c5f0abb71f00f37970588c4d57
Aj Oks <purelilyit@yahoo.com>
2020-06-28 00:21:05 +0100

    portfolio seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "All the tasks",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Etiam maximus, felis at lacinia congue, diam purus dictum enim,
+              ut dapibus dui augue vel mi. Aliquam erat volutpat.",
+        main_image: "https://via.placeholder.com/600x400",
+        thumb_image: "https://via.placeholder.com/350x200"
+    )
+end
