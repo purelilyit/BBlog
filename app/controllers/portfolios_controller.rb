f2831470b0739385610341fc8e684011317d9b9b
Aj Oks <purelilyit@yahoo.com>
2020-06-29 12:13:46 +0100

    portfolio item 11 deleted

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,5 +41,12 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
-
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 end
