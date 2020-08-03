36ad2ff0975f937ff3d606fd287e41811cde907c
Aj Oks <purelilyit@yahoo.com>
2020-08-03 19:41:43 +0100

    merged permissions branch

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -2,10 +2,15 @@ module CurrentUser
   extend ActiveSupport::Concern
 
   def current_user
-      super || OpenStruct.new(name: "Guest User",
-                              first_name: "Guest",
-                              last_name: "User",
-                              email: "guestuser@asdf.com"
-                             )
-    end
+    super || guest_user
+  end
+
+  def guest_user
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guestuser@asdf.com"
+    guest
+  end
 end
