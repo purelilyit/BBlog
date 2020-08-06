9a0223e037cd65ab9d6b8e3b69ff24421b03d63b
Aj Oks <purelilyit@yahoo.com>
2020-08-06 21:06:50 +0100

    permissions updated

---
+++ app/models/concerns/current_user_concern.rb
@@ -0,0 +1,12 @@
+def current_user
+  super || guest_user
+end
+
+def guest_user
+  guest = GuestUser.new
+  guest.name = "Guest User"
+  guest.first_name = "Guest"
+  guest.last_name = "User"
+  guest.email = "guestuser@asdf.com"
+  guest   
+end
