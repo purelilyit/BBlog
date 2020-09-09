a8154920e14d5b2cfa5e67be9244e3ccf394d3c3
Aj Oks <purelilyit@yahoo.com>
2020-09-09 12:49:44 +0100

    basic portfolios ASC function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_202537) do
+ActiveRecord::Schema.define(version: 2020_09_09_114333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_202537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "badge"
+    t.integer "position"
   end
 
   create_table "technologies", force: :cascade do |t|
