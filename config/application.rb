426d228e747888f4f01b391ebdd7e7746c176204
Aj Oks <purelilyit@yahoo.com>
2020-09-22 00:39:01 +0100

    added video to site background

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module BBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     # config.load_defaults 5.2
+      config.assets.paths << "#{Rails.root}/app/assets/videos"
 
       config.generators do |g|
         g.orm             :active_record
